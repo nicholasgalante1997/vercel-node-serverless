008b3a8892a40cec6d5cd26461410f7625478128
Nick G <nicholasgalante1997@gmail.com>
2021-11-05 22:25:41 -0600

    uuid

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import { v4 } from 'uuid';
 
 export default (request: VercelRequest, response: VercelResponse) => {
+  console.log(`[info]::[${new Date().toISOString}]:: request id:${v4()}`)
   response.status(200);
   response.setHeader('Content-Type', 'text/html');
   response.send('<em style="color:red;font-size:1rem;">powered by vercel</em>')
